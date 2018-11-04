113552cf10533cb4805728e5d2aa859bd688c5ba
Nicolas MAUGE <maugenicolas@gmail.com>
2018-11-04 12:30:50 +0100

    rename cifar directory

--- wikipedia/02-extract_text.py
+++ wikipedia/02-extract_text.py
@@ -5,6 +5,7 @@ from clean_wiki.wiki_fr_basic_clean import clean_text
 from clean_wiki.wiki_xml_to_text import process_xml_dump
 from utils.utils import hms_string
 from tqdm import tqdm
+import random
 
 out_dir = "data/articles/"
 ENCODING = "utf-8"
@@ -27,14 +28,15 @@ def get_corpus_text(n_tokens=150000000):
 				print("The end of the XML dump has been reached")
 				break
 
-			n_lines, cleaned_text = clean_text(text)
+			if random.random()<0.1: # whether to choose this article
+				n_lines, cleaned_text = clean_text(text)
 
-			if n_lines > 50: # since this text will be used for ML
-				n_articles_collected = n_articles_collected+1
-				delta_words = len(cleaned_text.split(" "))
-				n_words = n_words + delta_words
-				articles.write(cleaned_text + "\n")
-				pbar.update(delta_words)	
+				if n_lines > 50: # since this text will be used for ML
+					n_articles_collected = n_articles_collected+1
+					delta_words = len(cleaned_text.split(" "))
+					n_words = n_words + delta_words
+					articles.write(cleaned_text + "\n")
+					pbar.update(delta_words)	
 
 		print(f"{n_words} tokens collected in {n_articles_collected} articles")
 		pbar.close()
